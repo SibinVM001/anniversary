814de33b7b069cc1d53a75a08d25161f95f5d182
Sibin <sibin.limenzy@gmail.com>
2023-09-05 14:58:36 +0530

    test

--- checkResult.php
+++ checkResult.php
@@ -53,7 +53,7 @@
 
         function changeCookies($arrStr, $oldkey) {
             $content = str_replace('"'.$oldkey.'":', '"'.setCookies().'":', $arrStr);
-            $fp = fopen($_SERVER['DOCUMENT_ROOT'] . "/anniversary/sample.txt","wb");
+            $fp = fopen($_SERVER['DOCUMENT_ROOT'] . "/anniversary/sample.txt", 'r');
             fwrite($fp, $content);
             fclose($fp);  
         }
